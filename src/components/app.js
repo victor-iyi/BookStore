1010707fb669df8b22d9e97caba62787abfaaa03
victor-iyiola <javafolabi@gmail.com>
2017-07-04 18:06:11 +0100

    One shot baby!
    Coded all on one shot no errors! 
    Yay!!! :) :)

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h2>Book Store</h2>
+        <BookList />
+      </div>
     );
   }
 }
---
+++ src/reducers/books-reducer.js
@@ -0,0 +1,26 @@
+export default () => {
+	return [
+		{ key: 1, 
+			title: 'C++: How to Program' 
+		},
+		{ key: 2, 
+			title: 'Learn JavaScript the Hardway' 
+		},
+		{ key: 3, 
+			title: 'Java: You don\'t know shit' 
+		},
+		{ key: 4, 
+			title: 'Web development with PHP' 
+		},
+		{ key: 5, 
+			title: 'JavaScript: The Good Parts' 
+		},
+		{ key: 6, 
+			title: 'Linux for Dummies' 
+		},
+		{ key: 7, 
+			title: 'Classroom in a Book' 
+		},
+		
+	]
+}
